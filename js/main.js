b34d0ef7a580cf0719739b780abbb37782ed5aa1
AbduvahobovaMadina <madinaabduvahobova565@gmail.com>
2024-06-04 15:17:11 +0500

    js-002

---
+++ js/main.js
@@ -0,0 +1,73 @@
+// {
+//     // masala 1
+//     let a = prompt("a sonini kirting")
+//     let p = 4 * a
+//     alert ( `Perimetri: ${ p }`)
+// }
+
+// {
+//     // masala 2
+//     let a = prompt("a sonini kirting")
+//     let S = a * a
+//     alert ( `Yuzasi: ${ S }`)
+// }
+
+// {
+//     // masala 3
+//     let a = +prompt("a sonini kiriting")
+//     console.log(a);
+//     let b = +prompt("b sonini kirting")
+//     console.log(b);
+//     let S = a * b
+//     console.log("Yuzasi:" + S);
+//     let P = 2 * (a + b)
+//     console.log("Perimetri:" + P);
+//     alert ( `Yuzasi: ${S}  Perimetri: ${P}`)
+// }
+
+// {
+//     // masala 4
+//     let d = prompt("d sonini kirting")
+//     console.log(d);
+//     console.log("pi=" + 3.14);
+//     let L = 3.14 * d
+//     alert (`Uzunligi: ${L}`)
+// }
+
+// {
+//     // masala 5
+//     let a =prompt ("a sonini kirting")
+//     console.log(a);
+//     let V = a * a * a
+//     console.log("Hajmi:" + V);
+//     let S = 6 * a * a
+//     console.log("Yuzasi:" + S);
+//     alert (`Hajmi: ${V} Yuzasi: ${S}`)
+// }
+
+// {
+//     // masal 6
+//     let a = +prompt("a sonini kirting")
+//     let b = +prompt("b sonini kirting")
+//     let c = +prompt("c sonini kirting")
+//     let V = a * b * c
+//     let S = 2*(a*b+b*c+c*a)
+//     alert (`Hajmi: ${V} Yuzasi: ${S}`)
+// }
+
+// {
+//     // masala 7
+//     let R = prompt("R sonini kiriting")
+//     console.log("pi=" + 3.14);
+//     let L = 2 * 3.14 * R
+//     let S = 3.14 * R * R 
+//     alert (`Uzunligi: ${L} Yuzasi: ${S}`)
+// }
+
+// {
+//     // masala 8
+//     let a = +prompt("a sonini kiriting")
+//     let b = +prompt("b sonini kiriting")
+//     let M = (a + b)/2
+//     alert (`O'rta arifmetigi: ${M}`)
+// }
